d0295561510f49e78fd44de43941b61ed6391bdb
preqel <358795715@qq.com>
2020-05-05 08:55:52 +0800

    加入ffmpeg库并整合开发环境

--- app/src/main/jni/main.cpp
+++ app/src/main/jni/main.cpp
@@ -1,10 +1,14 @@
 #include "jni_example_com_testjni6_JNITest.h"
 #include <string>
-extern "C"
+extern "C" {
+#include "include/libavcodec/avcodec.h"
+}
 JNIEXPORT jstring JNICALL
 JNICALL Java_jni_example_com_testjni6_JNITest_getString(
         JNIEnv* env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
+
+   // std::string hello = "Hello from C++ a";
+    std::string hello = avcodec_configuration();
     return env->NewStringUTF(hello.c_str());
 }
